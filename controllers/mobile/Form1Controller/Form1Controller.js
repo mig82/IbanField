6e14e2712e23ed4a5ae4681b4cd99564f4dfe27f
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-09-02 18:22:17 +0200

    Expose and test iban value property

--- controllers/mobile/Form1Controller/Form1Controller.js
+++ controllers/mobile/Form1Controller/Form1Controller.js
@@ -1,12 +1,26 @@
 define(function(){
 	return {
-		postShow: function(){
+		assignIbanComponentEvents: function(){
 			this.view.accountField1.onValidIban = (iban)=>{
 				kony.print(`${iban} is a valid IBAN`);
 			};
 			this.view.accountField1.onInvalidIban = (iban)=>{
 				kony.print(`${iban} is NOT a valid IBAN`);
 			};
+		},
+
+		setTestValue: function(value){
+			this.view.accountField1.value = value;
+		},
+
+		bindTestButtons: function(){
+			this.view.testAccount1Button.onTouchEnd = ()=>{this.setTestValue("ES26 4141 5000 3000 1210 2699");};
+			this.view.testAccount2Button.onTouchEnd = ()=>{this.setTestValue("GB86 UBKL 1234 5698 7654 32");};
+		},
+
+		postShow: function(){
+			this.assignIbanComponentEvents();
+			this.bindTestButtons();
 		},
 		onNavigate: function(){
 			this.view.postShow = this.postShow;
