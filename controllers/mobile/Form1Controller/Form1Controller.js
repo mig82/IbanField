0eaa42c90ff07522ba39465c1a24b2fecbcfe69e
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-09-02 14:49:56 +0200

    Expose custom events for valid/invalid value

--- controllers/mobile/Form1Controller/Form1Controller.js
+++ controllers/mobile/Form1Controller/Form1Controller.js
@@ -1,3 +1,15 @@
-define({
-
+define(function(){
+	return {
+		postShow: function(){
+			this.view.accountField1.onValidIban = (iban)=>{
+				kony.print(`${iban} is a valid IBAN`);
+			};
+			this.view.accountField1.onInvalidIban = (iban)=>{
+				kony.print(`${iban} is NOT a valid IBAN`);
+			};
+		},
+		onNavigate: function(){
+			this.view.postShow = this.postShow;
+		}
+	};
 });
--- userwidgets/com.mig82.IbanField/modules/require/IbanFieldController.js
+++ userwidgets/com.mig82.IbanField/modules/require/IbanFieldController.js
@@ -1,8 +1,29 @@
 define(["./isValidIban"], function(isValidIban) {
 
 	return {
-		constructor: function(/*baseConfig, layoutConfig, pspConfig*/) {
+		validateIban: function(){
+			var iban = this.view.ibanTextBox.text;
+			if(isValidIban(iban)){
+				if(typeof this.onValidIban === "function"){
+					this.onValidIban(iban);
+				}
+			}
+			else{
+				if(typeof this.onInvalidIban === "function"){
+					this.onInvalidIban(iban);
+				}
+			}
+		},
 
+		preShow: function(){},
+
+		postShow: function(){
+			this.view.ibanTextBox.onEndEditing = this.validateIban;
+		},
+
+		constructor: function(/*baseConfig, layoutConfig, pspConfig*/) {
+			this.view.preShow = this.preShow;
+			this.view.postShow = this.postShow;
 		},
 		//Logic for getters/setters of custom properties
 		initGettersSetters: function() {
