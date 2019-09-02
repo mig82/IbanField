3551520e39f2566fa5f15c5fc04079f52488ca0a
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-09-02 14:16:35 +0200

    Define logic to validate IBAN

--- userwidgets/com.mig82.IbanField/modules/require/IbanFieldController.js
+++ userwidgets/com.mig82.IbanField/modules/require/IbanFieldController.js
@@ -1,3 +1,12 @@
-define({
-	
+define(["./isValidIban"], function(isValidIban) {
+
+	return {
+		constructor: function(/*baseConfig, layoutConfig, pspConfig*/) {
+
+		},
+		//Logic for getters/setters of custom properties
+		initGettersSetters: function() {
+
+		}
+	};
 });
