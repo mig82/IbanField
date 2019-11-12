47c730fd2b7c8b03e912dafa2342e4d632cf8ba1
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-11-12 18:08:53 +0100

    Add labels for invalid IBAN message

--- controllers/mobile/Form1Controller/Form1Controller.js
+++ controllers/mobile/Form1Controller/Form1Controller.js
@@ -4,11 +4,17 @@ define(function(){
 	return {
 		assignIbanComponentEvents: function(){
 			acctFields.forEach((field) => {
+
+				var fieldNum = field[field.length-1]; //1 or 2;
+				var label = this.view["errorLabel" + fieldNum];
+
 				this.view[field].onValidIban = (iban)=>{
 					kony.print(`Account ${iban} is a valid IBAN`);
+					label.opacity = 0;
 				};
 				this.view[field].onInvalidIban = (iban)=>{
 					kony.print(`Account ${iban} is NOT a valid IBAN`);
+					label.opacity = 1;
 				};
 			});
 		},
@@ -25,11 +31,16 @@ define(function(){
 			this.view.testAccount2Button.onTouchEnd = ()=>{this.setTestValue("GB86 UBKL 1234 5698 7654 32");};
 		},
 
+		preShow: function(){
+			this.view.errorLabel1.opacity = 0;
+			this.view.errorLabel2.opacity = 0;
+		},
 		postShow: function(){
 			this.assignIbanComponentEvents();
 			this.bindTestButtons();
 		},
 		onNavigate: function(){
+			this.view.preShow = this.preShow;
 			this.view.postShow = this.postShow;
 		}
 	};
